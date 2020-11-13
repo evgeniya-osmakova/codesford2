18c2adc4ad6c4081fa15a3c647954194c5665285
Evgeniya Osmakova <evgeniya.osmakova@gmail.com>
2020-11-13 09:32:06 +0300

    fix language

--- src/index.js
+++ src/index.js
@@ -2,6 +2,8 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './components/App.js';
 import './i18n.js';
+import { I18nextProvider } from 'react-i18next';
+import i18n from './i18n';
 import {createStore, applyMiddleware, compose} from 'redux';
 import reducers from './reducers/index';
 import { Provider } from 'react-redux';
@@ -18,10 +20,12 @@ const store = createStore(
 /* eslint-enable */
 
 ReactDOM.render(
-  <Provider store={store}>
-    <React.StrictMode>
-      <App />
-    </React.StrictMode>
-  </Provider>,
+    <Provider store={store}>
+      <React.StrictMode>
+        <I18nextProvider i18n={i18n}>
+          <App />
+        </I18nextProvider>
+      </React.StrictMode>
+    </Provider>,
   document.getElementById('root'),
 );
