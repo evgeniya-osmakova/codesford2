2948c69e849c7f695ec6740fa892255282b9a211
Evgeniya Osmakova <evgeniya.osmakova@gmail.com>
2020-11-13 09:54:03 +0300

    fix language

--- src/i18n.js
+++ src/i18n.js
@@ -4,10 +4,10 @@ import LanguageDetector from 'i18next-browser-languagedetector';
 import { initReactI18next } from 'react-i18next';
 
 i18n
-  .use(Backend)
   // detect user language
   // learn more: https://github.com/i18next/i18next-browser-languageDetector
   .use(LanguageDetector)
+  .use(Backend)
   // pass the i18n instance to react-i18next.
   .use(initReactI18next)
   // init i18next
